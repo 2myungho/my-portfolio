aa48ff644ca29d5a8a5aa831e9c2b78d73d86f38
2300020 <2300020@partner.skcc.com>
2023-08-23 17:58:03 +0900

    feat: tooltip 적용

--- src/app/section/Skill.tsx
+++ src/app/section/Skill.tsx
@@ -1,6 +1,58 @@
-import React from 'react'
+'use client'
+
+import React, { useState, ReactElement } from 'react'
+import { FaRegQuestionCircle } from 'react-icons/fa';
 
 import styles from '../Home.module.css';
+
+interface TooltipProps {
+  text: string[];
+  children: ReactElement;
+}
+
+export const Tooltip = ({ text, children }: TooltipProps) => {
+  const [showTooltip, setShowTooltip] = useState(false);
+
+  return (
+    <div className="relative">
+      {showTooltip && (
+          <div
+            style={{
+              position: 'absolute',
+              bottom: '-100%',
+              left: 60,
+              padding: '8px 12px',
+              backgroundColor: 'black',
+              color: 'white',
+              borderRadius: '6px',
+              zIndex: 1,
+              fontSize: '0.8rem',
+              width: '200px',
+            }}
+          >
+            {text.map((line, index) => (
+              <div key={index}>{line}</div>
+            ))}
+             <div style={{
+                position: 'absolute',
+                top: '50%',
+                left: -6,
+                width: '0',
+                height: '0',
+                borderTop: '8px solid transparent',
+                borderBottom: '8px solid transparent',
+                borderRight: '8px solid black',
+                transform: 'translateY(-50%)'
+                }}
+              />
+          </div>
+      )}
+      <div onMouseOver={() => setShowTooltip(true)} onMouseOut={() => setShowTooltip(false)} >
+        {children}
+      </div>
+    </div>
+  );
+}
 
 interface ArticleType {
   title: string;
@@ -11,12 +63,10 @@ const frontEnd: ArticleType = {
   title: 'Front-end',
   items: ['React.js 3', 'Next.js 2', 'Vue.js 2', 'Javascript 3', 'Html / Css 3', 'React Native 2']
 }
-
 const backEnd: ArticleType = {
   title: 'Back-end',
   items: ['Node.js 2', 'Typescript 2', 'Express 2', 'Java 1', 'Spring Boot 1']
 }
-
 const etc: ArticleType = {
   title: 'ETC',
   items: ['Git', 'Jira', 'Confluence', 'AWS', 'Docker', 'Firebase', 'Zeplin', 'Figma']
@@ -52,6 +102,13 @@ export default function Skill() {
       <div className={styles.sectionTitle}>
         <div className={styles.bulletSquare} />
         <h2 >Skills</h2>
+        <Tooltip text={['1: 기초 수준', '2: 사이드 프로젝트 개발 수준', '3: Production 개발 가능 수준']}>
+          <FaRegQuestionCircle
+            size="1.5rem"
+            color="#1664FF"
+            style={{marginLeft: '1rem', cursor: 'pointer'}}
+          />
+        </Tooltip>
       </div>
       <ArticleDisplay article={frontEnd} />
       <ArticleDisplay article={backEnd} />
