2c1d5ce7e90b96a163b89c251ad441e73ad60080
2300020 <2300020@partner.skcc.com>
2023-08-24 10:34:06 +0900

    style: 프로젝트 내용 변경

--- src/app/section/Project.tsx
+++ src/app/section/Project.tsx
@@ -138,15 +138,18 @@ export default function Project() {
           <br />
           <div className="lg:flex gap-x-10">
             <div className="w-full">
-              <div>계획 관리형 SNS 서비스입니다. 계획들의 행성이라는 의미에 Planit 서비스는 계획을 공유하며 동기부여 및 능률을 향상시키고, 더 많은 사람들의 계획을 돕기 위해 제작되었습니다.</div>
+              <div>계획 관리형 SNS 서비스입니다. 계획들의 행성이라는 의미의 Planit 서비스는 계획을 공유하며 동기부여 및 능률을 향상시키고, 더 많은 사람들의 계획을 돕기 위해 제작되었습니다.</div>
               <br />
               <div>해당 프로젝트에서 주로 프론트엔드 부분을 담담하고, 서버 보조 및 배포에 참여했습니다.</div>
-              <div>서버는 MSA구조로 설계하였으며, AWS를 활용해 배포를 진행했습니다.</div>
-              <div>담당한 기능: </div>
-              <div>- Group 서비스 개발</div>
-              <div>- S3 파일 업로드 서비스 개발</div>
-              <div>- 프로젝트 배포 참여</div>
-              <div>- 풀스택 개발</div>
+              <div className="mb-1">서버는 MSA구조로 설계하였으며, AWS를 활용해 배포를 진행했습니다.</div>
+              <div className="mb-1">
+                <div>담당한 기능: </div>
+                <div>- Group 서비스 개발</div>
+                <div>- S3 파일 업로드 서비스 개발</div>
+                <div>- 프로젝트 배포 참여</div>
+                <div>- 풀스택 개발</div>
+              </div>
+              <div>자세한 내용은 Git README에서 확인할 수 있습니다.</div>
               <br />
               <div>Git: <a href="https://github.com/2myungho/Planit-Project"> Git README </a></div>
               <div>Tech: React, Typescript, Java, SpringBoot, MariaDB, AWS, Docker</div>
@@ -170,7 +173,7 @@ export default function Project() {
             <div className="w-full">
               <div>이탈리아어로 Lavita는 &quot;풍요로운 삶&quot;을 뜻합니다. 직장인들을 타겟으로 정했으며, 직장인들이 질 좋은 커피로 삶이 조금 더 풍요로워 졌으면 하는 마음으로 제작했습니다.</div>
               <br />
-              <div  className="mb-1">학습한 내용이 생길 때마다 직접 적용해보던 개인 프로젝트입니다.</div>
+              <div className="mb-1">학습한 내용이 생길 때마다 직접 적용해보던 개인 프로젝트입니다.</div>
               <div className="mb-2">
                 <div>1차 진행:</div>
                 <div>-  Html, CSS, Javascript, JQuery 를 이용하여 홈페이지 폼 제작</div>
@@ -179,12 +182,13 @@ export default function Project() {
                 <div>2차 진행: </div>
                 <div>- 학습한 Spring Framework, Jsp, Oracle 등을 적용하여 서버 구현</div>
               </div>
-              <div>
+              <div className="mb-2">
                 <div>3차 진행: </div>
                 <div>- AWS EC2서버를 활용해 프로젝트 배포하기</div>
                 <div>- AWS RDS를 EC2 서버에서 연동</div>
                 <div>- RDS에서 Oracla이 유료이기 때문에 MariaDB로 변경</div>
               </div>
+              <div>자세한 내용은 Git README에서 확인할 수 있습니다.</div>
               <br />
               <div>Git: <a href="https://github.com/2myungho/CoffeeLavita_Project"> Git README </a></div>
               <div>Tech: Html/Css, Javascript JQuery, Java, Spring Framework, Jsp, MariaDB, AWS</div>
