4ed3f1fac181750ab452a0715adbedf242e54f32
2myungho <dl_audgh@naver.com>
2024-01-07 03:22:26 +0900

    modified: introduce 수정

--- src/app/section/Introduce.tsx
+++ src/app/section/Introduce.tsx
@@ -5,26 +5,38 @@ import styles from "../Home.module.css";
 export default function Introduce() {
   return (
     <section className={styles.section}>
-      <div className="text-3xl font-semibold">안녕하세요.</div>
-      <div className="text-3xl font-semibold">
-        프론트엔드 엔지니어 이명호입니다!
+      <div className={styles.sectionTitle}>
+        <div className={styles.bulletSquare} />
+        <h2>Introduce</h2>
       </div>
-      <br />
-      <div className="text-2xl">
-        3년차 프론트엔드 엔지니어로 근무하고 있으며, 서비스 개발에 큰 관심을
-        가지고 있습니다.
-      </div>
-      <div className="text-2xl">
-        항상 자기개발을 멈추지 않으려고 노력하고 있으며, 최근에는 앱 서비스
-        개발에 관심이 생겨,
-      </div>
-      <div className="text-2xl">
-        저의 취미인 운동을 주제로 한 앱을 React Native로 개발하고 지속적으로
-        업데이트하고 있습니다.
-      </div>
-      <div className="text-2xl">
-        다양한 개발자들과 기술적 경험을 공유하고 서로 성장할 수 있는 환경을
-        기대합니다.
+      <div className="bg-[#f6f6f6] rounded-md p-4 md:p-8 lg:py-12 lg:px-16 ">
+        <div>안녕하세요.</div>
+        <div>3년차 프론트엔드 엔지니어 이명호입니다.</div>
+        <br />
+        <div>
+          제품 개발팀의 프론트엔드 엔지니어로 근무하며, 필요에 따라 BFF(Backend
+          for Frontend) 서버 업무도 담당했습니다. 다양한 사내 제품 개발과
+          유지보수를 맡아, 새 프로젝트의 구축부터 기존 프로젝트의 기능 개발까지
+          다양한 업무를 수행했습니다. 주 업무 중 하나는 데이터 분석가들이
+          학습시킨 모델로부터 얻은 시계열 데이터를 다양한 차트 형태로 시각화하는
+          서비스의 개발이었습니다. 이 과정에서 사용자 경험 향상을 위해 성능
+          최적화와 코드 효율성에 중점을 두고 작업했습니다.
+        </div>
+        <br />
+        <div>
+          요즘 제 취미인 운동을 주제로 한 '헛둘' 이라는 모바일 어플리케이션을
+          React Native로 개발하고 사용자 피드백을 받아 지속해서 관리하고
+          있습니다. 사용자들과 소통하는 것에 굉장한 보람을 느끼며, 어떻게 하면
+          사용자들이 즐겁게 사용할 수 있을지, 사용에 불편한 점은 없는지 등에
+          대한 문제 해결을 지속적으로 고민하고 있습니다. 이러한 인사이트를 앱의
+          기획과 개발 과정에 반영하고 사용자 경험에 더욱 신경 쓰며 개발하고
+          있습니다.
+        </div>
+        <br />
+        <div>
+          다양한 배경을 가진 개발자들과 함께 기술적 경험을 공유하고, 서로 성장을
+          돕는 환경에서 근무하기를 기대합니다.
+        </div>
       </div>
     </section>
   );
